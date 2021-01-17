7acebb93ce189ca950e08744510bd626ab4a401c
Jules Thomas D <48414478+julowayne@users.noreply.github.com>
2021-01-17 15:21:10 +0100

    added icon to conversation in message

--- components/message.js
+++ components/message.js
@@ -21,7 +21,7 @@ const message = {
   },
   template: `
   <div class="col-md-7 border-left">
-    <h4 class="text-center mb-3 p-3"><strong>Conversation</strong></h4>
+    <h4 class="text-center mb-3 p-3"><strong>Conversation <span class="mdi mdi-18px mdi-forum"></span></strong></h4>
     <h5><strong>{{ loggedUser }}</strong></h5>
     <div class="d-flex m-2 p-2 bg-info w-50 shadow-sm rounded justify-content-between" v-for="message in messages"><strong> {{ message }} </strong>
     <span>
@@ -29,8 +29,6 @@ const message = {
       <span class="mdi mdi-18px mdi-check"></span>
      </span>
     </div>
-
-
     <form @submit.prevent="sendMessage">
     <div class="input-group mb-3">
       <input type="text" class="form-control shadow-sm" v-model="message" placeholder="Type your message here ..." aria-describedby="basic-addon2">
